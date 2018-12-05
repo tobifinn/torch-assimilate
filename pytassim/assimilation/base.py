3bb00c5907e67aa41fe7c1c165aa6864ec72aaaf
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2018-12-05 13:28:09 +0100

    Added a docstring to assimilate

--- pytassim/assimilation/base.py
+++ pytassim/assimilation/base.py
@@ -36,9 +36,44 @@ logger = logging.getLogger(__name__)
 
 
 class BaseAssimilation(object):
-    def __init__(self, pre_infl=None, post_infl=None, localization=None):
+    def __init__(self):
         pass
 
     @abc.abstractmethod
-    def assimilate(self):
+    def assimilate(self, state, observations, analysis_time=None):
+        """
+        This assimilate the ``observations`` in given background ``state`` and
+        creates an analysis for given ``analysis_time``. The observations need
+        an observation operator, which translate given state into an
+        observation-equivalent. The state, observations, observation covariance
+        and and the observation-equivalent are used to update given state.
+
+        Parameters
+        ----------
+        state : :py:class:`xarray.DataArray`
+            This state is used to generate an observation-equivalent. It is
+            further updated by this assimilation algorithm and given
+            ``observation``. This :py:class:`~xarray.DataArray` should have
+            four coordinates, which are specified in
+            :py:class:`pytassim.state.ModelState`.
+        observations : :py:class:`xarray.Dataset` or \
+        iterable(:py:class:`xarray.Dataset`)
+            These observations are used to update given state. An iterable of
+            many :py:class:`xarray.Dataset` can be used to assimilate different
+            variables. For the observation state, these observations are
+            stacked such that the observation state contains all observations.
+            The :py:class:`xarray.Dataset` are validated with
+            :py:class:`pytassim.observation.Observation.valid`
+        analysis_time : :py:class:`datetime.datetime` or None, optional
+            This analysis time determines at which point the state is updated.
+            If the analysis time is None, than the last time point in given
+            state is used.
+
+        Returns
+        -------
+        analysis : :py:class:`xarray.DataArray`
+            The analysed state based on given state and observations. The
+            analysis has same coordinates as given ``state`` except ``time``,
+            which contains only one time step.
+        """
         pass
