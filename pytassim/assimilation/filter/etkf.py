066df4b73d430454fd29846e26188807efb3e6be
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2019-02-14 15:49:24 +0100

    outsourced etkf core functions

--- pytassim/assimilation/filter/etkf.py
+++ pytassim/assimilation/filter/etkf.py
@@ -35,6 +35,7 @@ import numpy as np
 # Internal modules
 import pytassim.state
 from .filter import FilterAssimilation
+from .etkf_core import gen_weights
 
 logger = logging.getLogger(__name__)
 
@@ -124,8 +125,9 @@ class ETKFilter(FilterAssimilation):
         prepared_states = self._prepare(
             state, observations
         )[:-1]
-        torch_states = self._states_to_torch(*prepared_states)
-        w_mean, w_perts = self._gen_weights(*torch_states)
+        innov, hx_perts, obs_cov = self._states_to_torch(*prepared_states)
+        back_prec = self._get_back_prec(len(state.ensemble))
+        w_mean, w_perts = gen_weights(innov, hx_perts, obs_cov, back_prec)
         state_mean, state_perts = state.state.split_mean_perts()
         analysis = self._apply_weights(w_mean, w_perts, state_mean, state_perts)
         analysis = analysis.transpose('var_name', 'time', 'ensemble', 'grid')
@@ -179,15 +181,16 @@ class ETKFilter(FilterAssimilation):
         hx_mean, hx_perts, filtered_obs = self._prepare_back_obs(state,
                                                                  observations)
         obs_state, obs_cov, obs_grid = self._prepare_obs(filtered_obs)
-        self._set_back_prec(len(state.ensemble))
         innov = obs_state - hx_mean
         return innov, hx_perts, obs_cov, obs_grid
 
-    def _set_back_prec(self, ens_mems):
-        self._back_prec = torch.eye(ens_mems, dtype=self.dtype)
+    def _get_back_prec(self, ens_mems):
+        back_prec = torch.eye(ens_mems, dtype=self.dtype)
         if self.gpu:
-            self._back_prec = self._back_prec.cuda()
-        self._back_prec *= (ens_mems - 1)
+            back_prec = back_prec.cuda()
+        back_prec *= (ens_mems - 1)
+        back_prec /= self.inf_factor
+        return back_prec
 
     def _prepare_back_obs(self, state, observations):
         pseudo_obs, filtered_obs = self._apply_obs_operator(state, observations)
@@ -207,128 +210,6 @@ class ETKFilter(FilterAssimilation):
         pseudo_obs_concat = xr.concat(state_stacked_list, dim='obs_id')
         hx_mean, hx_perts = pseudo_obs_concat.state.split_mean_perts()
         return hx_mean.values, hx_perts.T.values
-
-    def _compute_c(self, hx_perts, obs_cov, obs_weights=1):
-        if torch.allclose(obs_cov, torch.diag(torch.diagonal(obs_cov))):
-            calculated_c = self._compute_c_diag(hx_perts, obs_cov)
-        else:
-            calculated_c, _ = self._compute_c_chol(hx_perts, obs_cov)
-        calculated_c = calculated_c * obs_weights
-        return calculated_c
-
-    @staticmethod
-    def _compute_c_diag(hx_perts, obs_cov):
-        calculated_c = hx_perts.t() / torch.diagonal(obs_cov)
-        return calculated_c
-
-    @staticmethod
-    def _compute_c_chol(hx_perts, obs_cov, alpha=0):
-        obs_cov_prod = torch.matmul(obs_cov.t(), obs_cov)
-        obs_hx = torch.matmul(obs_cov.t(), hx_perts)
-        mat_size = obs_cov_prod.size()[1]
-        step = mat_size + 1
-        end = mat_size * mat_size
-        calculated_c = None
-        while calculated_c is None:
-            try:
-                mat_upper = torch.cholesky(obs_cov_prod, upper=True)
-                calculated_c = torch.potrs(obs_hx, mat_upper, upper=True).t()
-            except RuntimeError:
-                obs_cov_prod.view(-1)[:end:step] -= alpha
-                if alpha == 0:
-                    alpha = 0.00001
-                else:
-                    alpha *= 10
-                obs_cov_prod.view(-1)[:end:step] += alpha
-        return calculated_c, alpha
-
-    def _calc_precision(self, c, hx_perts):
-        prec_obs = torch.matmul(c, hx_perts)
-        prec_ana = self._back_prec / self.inf_factor + prec_obs
-        return prec_ana
-
-    @staticmethod
-    def _det_square_root_eigen(evals_inv, evects, evects_inv):
-        ens_size = evals_inv.size()[0]
-        w_perts = ((ens_size - 1) * evals_inv) ** 0.5
-        w_perts = torch.matmul(evects, torch.diagflat(w_perts))
-        w_perts = torch.matmul(w_perts, evects_inv)
-        return w_perts
-
-    @staticmethod
-    def _eigendecomp(precision):
-        evals, evects = torch.symeig(precision, eigenvectors=True, upper=False)
-        evals[evals < 0] = 0
-        evals_inv = 1 / evals
-        evects_inv = evects.t()
-        return evals, evects, evals_inv, evects_inv
-
-    def _gen_weights(self, innov, hx_perts, obs_cov, obs_weights=1):
-        """
-        This method is the main method to calculates the ensemble weights,
-        based on [H07]_. To generate the weights, the given arguments have to be
-        prepared and in a special format. The weights are estimated with
-        PyTorch.
-
-        Parameters
-        ----------
-        innov : :py:class:`torch.tensor`
-            These innovations are multiplied by the ensemble gain to estimate
-            the mean ensemble weights. These innovation should have a shape of
-            :math:`l`, the observation length.
-        hx_perts : :py:class:`torch.tensor`
-            These are the ensemble perturbations in ensemble space. These
-            perturbations are used to calculated the analysed ensemble
-            covariance in weight space. These perturbations have a shape of
-            :math:`l~x~k`, with :math:`k` as ensemble size and :math:`l` as
-            observation length.
-        obs_cov : :py:class:`torch.tensor`
-            This tensor represents the observation covariance. This covariance
-            is used for the estimation of the analysed covariance in weight
-            space. The shape of this covariance should be :math:`l~x~l`, with
-            :math:`l` as observation length.
-        obs_weights : :py:class:`torch.tensor` or float, optional
-            These are the observation weights. These observation weights can be
-            used for localization or weighting purpose. If these observation
-            weights are a float, then the same weight for every observation is
-            used. If these weights are a :py:class:`~torch.tensor`, then the
-            shape of this tensor should be :math:`l`, the observation length.
-            The default values is 1, indicating that every observation is
-            uniformly weighted.
-
-        Returns
-        -------
-        w_mean : :py:class:`torch.tensor`
-            The estimated ensemble mean weights. These weights can be used to
-            update the ensemble mean. The shape of this tensor is :math:`k`, the
-            ensemble size.
-        w_perts : :py:class:`torch.tensor`
-            The estimated ensemble perturbations in weight space. These weights
-            can be used to estimate new centered ensemble perturbations. The
-            shape of this tensor is :math:`k~x~k`, with :math:`k` as ensemble
-            size.
-
-        References
-        ----------
-        .. [H07] Hunt, B. R., Kostelich, E. J., & Szunyogh, I. (2007).
-                 Efficient data assimilation for spatiotemporal chaos: A local
-                 ensemble transform Kalman filter. Physica D: Nonlinear
-                 Phenomena, 230(1), 112–126.
-                 https://doi.org/10.1016/j.physd.2006.11.008
-        """
-        estimated_c = self._compute_c(hx_perts, obs_cov, obs_weights)
-        prec_ana = self._calc_precision(estimated_c, hx_perts)
-        evd = self._eigendecomp(prec_ana)
-        evals, evects, evals_inv, evects_inv = evd
-
-        cov_analysed = torch.matmul(evects, torch.diagflat(evals_inv))
-        cov_analysed = torch.matmul(cov_analysed, evects_inv)
-
-        gain = torch.matmul(cov_analysed, estimated_c)
-        w_mean = torch.matmul(gain, innov)
-
-        w_perts = self._det_square_root_eigen(evals_inv, evects, evects_inv)
-        return w_mean, w_perts
 
     @staticmethod
     def _weights_matmul(perts, weights):
