686bb9828e20cf692885c9060d83346f127c33f2
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2019-02-15 12:23:05 +0100

    Finished multiprocessing based on pool

---
+++ pytassim/assimilation/filter/letkf_dist.py
@@ -0,0 +1,201 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Created on 2/14/19
+#
+# Created for torch-assimilate
+#
+# @author: Tobias Sebastian Finn, tobias.sebastian.finn@uni-hamburg.de
+#
+#    Copyright (C) {2019}  {Tobias Sebastian Finn}
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# System modules
+import logging
+import itertools
+
+# External modules
+import torch
+import numpy as np
+
+# Internal modules
+from . import etkf_core
+from .letkf import LETKFilter
+
+
+logger = logging.getLogger(__name__)
+
+
+def local_etkf(ind, innov, hx_perts, obs_cov, back_prec, obs_grid, state_grid,
+               state_perts, localization=None):
+    obs_weights = 1
+    if localization:
+        use_obs, obs_weights = localization.localize_obs(
+            state_grid[ind], obs_grid
+        )
+        obs_weights = torch.as_tensor(obs_weights[use_obs], dtype=innov.dtype)
+        use_obs = torch.ByteTensor(use_obs.astype(int))
+        if innov.is_cuda:
+            obs_weights = obs_weights.cuda()
+        innov = innov[use_obs]
+        hx_perts = hx_perts[use_obs]
+        obs_cov = obs_cov[use_obs, :][:, use_obs]
+    w_mean_l, w_perts_l = etkf_core.gen_weights(back_prec, innov, hx_perts,
+                                                obs_cov, obs_weights)
+    weights_l = (w_mean_l + w_perts_l).t()
+    ana_state_l = torch.matmul(state_perts[ind], weights_l)
+    return ana_state_l, weights_l, w_mean_l
+
+
+class DistributedLETKF(LETKFilter):
+    """
+    This is a MPI based implementation of the `localized ensemble transform
+    Kalman filter` [H07]_, which is a localized version of the `ensemble
+    transform Kalman filter` [B01]_. This method iterates independently over
+    each grid point in given background state. Given localization instance can
+    be used to constrain the influence of observations in space. The ensemble
+    weights are calculated for every grid point and independently applied to
+    every grid point. This implementation follows [H07]_ with local weight
+    estimation and is implemented in PyTorch. This implementation allows
+    filtering in time based on linear propagation assumption [H04]_ and ensemble
+    smoothing.
+
+    References
+    ----------
+    .. [B01] Bishop, C. H., Etherton, B. J., & Majumdar, S. J. (2001).
+             Adaptive sampling with the ensemble transform Kalman filter.
+             Part I: Theoretical aspects. Monthly Weather Review, 129(3),
+             420–436.
+    .. [H04] Hunt, B., et al. Four-dimensional ensemble Kalman filtering.
+             Tellus A, 56(4), 273–277.
+    .. [H07] Hunt, B. R., Kostelich, E. J., & Szunyogh, I. (2007).
+             Efficient data assimilation for spatiotemporal chaos: A local
+             ensemble transform Kalman filter. Physica D: Nonlinear
+             Phenomena, 230(1), 112–126.
+             https://doi.org/10.1016/j.physd.2006.11.008
+
+    Parameters
+    ----------
+    chunks : int, optional
+        The data is splitted up in this number of chunks.
+    localization : obj or None, optional
+        This localization is used to localize and constrain observations
+        spatially. If this localization is None, no localization is applied such
+        it is an inefficient version of the `ensemble transform Kalman filter`.
+        Default value is None, indicating no localization at all.
+    inf_factor : float, optional
+        Multiplicative inflation factor :math:`\\rho``, which is applied to the
+        background precision. An inflation factor greater one increases the
+        ensemble spread, while a factor less one decreases the spread. Default
+        is 1.0, which is the same as no inflation at all.
+    smoother : bool, optional
+        Indicates if this filter should be run in smoothing or in filtering
+        mode. In smoothing mode, no analysis time is selected from given state
+        and the ensemble weights are applied to the whole state. In filtering
+        mode, the weights are applied only on selected analysis time. Default
+        is False, indicating filtering mode.
+    gpu : bool, optional
+        Indicator if the weight estimation should be done on either GPU (True)
+        or CPU (False): Default is None. For small models, estimation of the
+        weights on CPU is faster than on GPU!.
+    """
+    def __init__(self, pool, chunksize=10, localization=None, inf_factor=1.0,
+                 smoother=True, gpu=False, pre_transform=None,
+                 post_transform=None):
+        super().__init__(localization, inf_factor, smoother, gpu, pre_transform,
+                         post_transform)
+        self._pool = None
+        self.pool = pool
+        self._chunksize = 1
+        self.chunksize = chunksize
+
+    @property
+    def pool(self):
+        return self._pool
+
+    @pool.setter
+    def pool(self, new_pool):
+        if hasattr(new_pool, 'starmap') and callable(new_pool.starmap):
+            self._pool = new_pool
+        else:
+            raise TypeError('Given distributed pool needs a '
+                            'itertools-like `starmap` method!')
+
+    def update_state(self, state, observations, analysis_time):
+        """
+        This method updates the state based on given observations and analysis
+        time. This method prepares different states, localize these states,
+        iterates over state grid points, calculates the ensemble  weights and
+        applies these weight to localized state. The calculation of the
+        weights is based on PyTorch, while everything else is calculated with
+        Numpy / Xarray.
+
+        Parameters
+        ----------
+        state : :py:class:`xarray.DataArray`
+            This state is used to generate an observation-equivalent. It is
+            further updated by this assimilation algorithm and given
+            ``observation``. This :py:class:`~xarray.DataArray` should have
+            four coordinates, which are specified in
+            :py:class:`pytassim.state.ModelState`.
+        observations : :py:class:`xarray.Dataset` or \
+        iterable(:py:class:`xarray.Dataset`)
+            These observations are used to update given state. An iterable of
+            many :py:class:`xarray.Dataset` can be used to assimilate different
+            variables. For the observation state, these observations are
+            stacked such that the observation state contains all observations.
+        analysis_time : :py:class:`datetime.datetime`
+            This analysis time determines at which point the state is updated.
+
+        Returns
+        -------
+        analysis : :py:class:`xarray.DataArray`
+            The analysed state based on given state and observations. The
+            analysis has same coordinates as given ``state``. If filtering mode
+            is on, then the time axis has only one element.
+        """
+        innov, hx_perts, obs_cov, obs_grid = self._prepare(state, observations)
+        back_state = state.transpose('grid', 'var_name', 'time', 'ensemble')
+        state_mean, state_perts = back_state.state.split_mean_perts()
+        back_prec = self._get_back_prec(len(back_state.ensemble))
+        innov, hx_perts, obs_cov, back_state = self._states_to_torch(
+            innov, hx_perts, obs_cov, state_perts.values,
+        )
+        innov, hx_perts, obs_cov, back_state, back_prec = self._share_states(
+            innov, hx_perts, obs_cov, back_state, back_prec
+        )
+        state_grid = state_perts.grid.values
+        grid_inds = range(len(state_grid))
+        zipped_args = zip(
+            grid_inds,
+            itertools.repeat(innov),
+            itertools.repeat(hx_perts),
+            itertools.repeat(obs_cov),
+            itertools.repeat(back_prec),
+            itertools.repeat(obs_grid),
+            itertools.repeat(state_grid),
+            itertools.repeat(back_state),
+            itertools.repeat(self.localization),
+        )
+        etkf_results = self.pool.starmap(local_etkf, zipped_args)
+        state_perts.values = np.stack([r[0].numpy() for r in etkf_results])
+        analysis = (state_mean+state_perts).transpose(*state.dims)
+        return analysis
+
+    @staticmethod
+    def _share_states(*states):
+        shared_states = [s.share_memory_() for s in states]
+        return shared_states
