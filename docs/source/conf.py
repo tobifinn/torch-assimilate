7ab79fa2c95e3b6b113640c2dbcb4a1a50f9a898
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2019-02-19 14:13:32 +0100

    Added sphinxcontrib.bibtex for bibliography

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,6 +48,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'sphinxcontrib.plantuml',
+    'sphinxcontrib.bibtex'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
