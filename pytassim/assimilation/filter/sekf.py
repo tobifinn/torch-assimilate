01bcb96d2f6bf3ad37651a63108cd98a6edd4beb
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2019-07-17 13:20:08 +0200

    Added function to estimate increments

--- pytassim/assimilation/filter/sekf.py
+++ pytassim/assimilation/filter/sekf.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Created on 7/16/19
+# Created on 7/17/19
 #
 # Created for torch-assimilate
 #
@@ -27,13 +27,76 @@
 import logging
 
 # External modules
+import torch
+import pandas as pd
 
 # Internal modules
-from ..base_sekf import BaseSEKF
+from .filter import FilterAssimilation
+from pytassim.utilities import chol_solve
 
 
 logger = logging.getLogger(__name__)
 
 
-class SEKF(BaseSEKF):
-    pass
+def estimate_inc(innov, h_jacob, cov_back, obs_err):
+    ht = h_jacob.transpose(-1, -2)
+    hb = torch.mm(h_jacob, cov_back)
+    innov_prec = torch.mm(hb, ht)
+    mat_size = innov_prec.size()[1]
+    step = mat_size + 1
+    end = mat_size * mat_size
+    innov_prec.view(-1)[:end:step] += torch.pow(obs_err, 2)
+    norm_innov = chol_solve(innov_prec, innov).t()
+    k_dist = torch.mm(cov_back, ht)
+    inc_ana = torch.mm(k_dist, norm_innov).squeeze(-1)
+    return inc_ana
+
+
+class SEKF(FilterAssimilation):
+    def __init__(self, b_matrix, h_jacob, smoother=True, gpu=False,
+                 pre_transform=None, post_transform=None, **kwargs):
+        super().__init__(
+            smoother=smoother, gpu=gpu, pre_transform=pre_transform,
+            post_transform=post_transform
+        )
+        self._b_matrix = None
+        self._h_jacob = None
+        self.b_matrix = b_matrix
+        self.h_jacob = h_jacob
+
+    def _prepare(self, pseudo_state, observations):
+        logger.info('Apply observation operator')
+        pseudo_obs, filtered_obs = self._prepare_back_obs(pseudo_state,
+                                                          observations)
+        logger.info('Concatenate observations')
+        obs_state, obs_cov, obs_grid = self._prepare_obs(filtered_obs)
+        innov = obs_state - pseudo_obs
+        return innov, pseudo_obs, obs_cov, obs_grid
+
+    @staticmethod
+    def get_horizontal_grid(state):
+        grid_index = state.get_index('grid')
+        hori_index = pd.MultiIndex.from_product(
+            grid_index.levels[:-1], names=grid_index.names[:-1]
+        )
+        return hori_index
+
+    def update_state(self, state, observations, pseudo_state, analysis_time):
+        innov, pseudo_obs, obs_cov, obs_grid = self._prepare(
+            pseudo_state, observations
+        )
+
+        grid_hori = self.get_horizontal_grid(state)
+        grid_iter = self.to_dask_array(grid_hori.values)
+
+        ana_incs = []
+        for k, grid_block in enumerate(grid_iter.blocks):
+            sel_innov = innov.sel(grid=grid_block)
+            sel_state = state.sel(grid=grid_block)
+            state_inc = dask.delayed(estimate_inc_chunkwise)(
+                sel_state, sel_innov
+            )
+            ana_incs.append(state_inc)
+        ana_incs = dask.delayed(da.concatenate)(ana_incs, axis=-1)
+        analysis = state + ana_incs.compute()
+        return analysis
