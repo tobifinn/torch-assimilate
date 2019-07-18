b2ceccd2939d6a197888fcdec3f1b63e2431d5e5
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2019-07-18 14:50:34 +0200

    Added SEKF to init

--- pytassim/assimilation/filter/__init__.py
+++ pytassim/assimilation/filter/__init__.py
@@ -1,6 +1,7 @@
 from .etkf import *
 from .letkf import *
+from .sekf import *
 from .letkf_dist import *
 
 __all__ = ['ETKFCorr', 'ETKFUncorr', 'LETKFUncorr', 'LETKFCorr',
-           'DistributedLETKFCorr', 'DistributedLETKFUncorr']
+           'SEKFCorr', 'SEKFUncorr']
