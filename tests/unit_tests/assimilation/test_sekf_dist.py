eb4066de1594df5915220038b9fad4df77193af9
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2019-07-19 15:47:14 +0200

    Added test for distributed SEKF

--- tests/unit_tests/assimilation/test_sekf_dist.py
+++ tests/unit_tests/assimilation/test_sekf_dist.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 """
-Created on 7/16/19
+Created on 7/19/19
 
 Created for torch-assimilate
 
@@ -31,10 +31,12 @@ import os
 import xarray as xr
 import numpy as np
 import pandas as pd
+import torch
 
 # Internal modules
-from pytassim.assimilation.variational.var_sekf import VarSEKF
-from pytassim.testing.dummy import dummy_h_jacob, dummy_obs_operator
+from pytassim.assimilation.filter.sekf import SEKFCorr
+from pytassim.assimilation.filter.sekf_dist import DistributedSEKFCorr
+from pytassim.testing import dummy_h_jacob, dummy_obs_operator, if_gpu_decorator
 from pytassim.testing.cases import DistributedCase
 
 
@@ -44,7 +46,7 @@ BASE_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 DATA_PATH = os.path.join(os.path.dirname(BASE_PATH), 'data')
 
 
-class TestVarSEKF(DistributedCase):
+class TestSEKFDistributed(DistributedCase):
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
@@ -56,13 +58,17 @@ class TestVarSEKF(DistributedCase):
 
     def setUp(self) -> None:
         self.b_matrix = np.identity(4) * 0.5
-        self.algorithm = VarSEKF(
-            b_matrix=self.b_matrix, h_jacob=dummy_h_jacob,
-            client=self.client, chunksize=10
+        self.algorithm = DistributedSEKFCorr(
+            client=self.client, b_matrix=self.b_matrix, h_jacob=dummy_h_jacob
         )
         self.state = self.verticalize_state(self.state, 4)
+        pseudo_state = self.state.sel(vgrid=0).copy()
+        self.pseudo_state = pseudo_state.rename({'lon': 'grid'})
         self.obs = self.obs.sel(obs_grid_1=(self.obs.obs_grid_1 % 4 == 0),
                                 obs_grid_2=(self.obs.obs_grid_2 % 4 == 0))
+        self.obs['obs_grid_1'] = self.obs.obs_grid_1 // 4
+        self.obs['obs_grid_2'] = self.obs.obs_grid_2 // 4
+        self.obs.obs.operator = dummy_obs_operator
 
     @ staticmethod
     def verticalize_state(state, vert_dim=4) -> xr.DataArray:
@@ -70,9 +76,23 @@ class TestVarSEKF(DistributedCase):
         vert_grid = state['grid'].values % vert_dim
         hori_grid = state['grid'].values // vert_dim
         zipped_grid = [t for t in zip(hori_grid, vert_grid)]
-        multi_grid = pd.MultiIndex.from_tuples(zipped_grid, names=['', ''])
+        multi_grid = pd.MultiIndex.from_tuples(
+            zipped_grid, names=['lon', 'vgrid']
+        )
         state['grid'] = multi_grid
         return state
+
+    def test_distributed_gives_same_solution_as_undistributed(self):
+        analysis = self.algorithm.assimilate(
+            self.state, (self.obs, ), self.pseudo_state
+        )
+
+        undistributed = SEKFCorr(b_matrix=self.b_matrix,
+                                 h_jacob=self.algorithm.h_jacob)
+        analysis_undist = undistributed.assimilate(
+            self.state, (self.obs, ), self.pseudo_state
+        )
+        xr.testing.assert_equal(analysis, analysis_undist)
 
 
 if __name__ == '__main__':
