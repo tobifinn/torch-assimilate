e51dd3e34a6ee1f1b99444afced69359064a55b3
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2020-08-11 19:26:30 +0200

    Revised distributed LETKF towards new structure with mixins

--- pytassim/assimilation/filter/letkf_dist.py
+++ pytassim/assimilation/filter/letkf_dist.py
@@ -27,95 +27,20 @@
 import logging
 
 # External modules
-from distributed import Client, wait, Future
-import dask
-import dask.array as da
+from distributed import Client
+import xarray as xr
 
 import torch
 
 # Internal modules
-from .letkf import LETKFCorr, localize_states
-from .etkf_core import gen_weights_uncorr
+from .etkf_core import _CorrMixin, _UnCorrMixin
+from .letkf import _LETKFBase
 
 
 logger = logging.getLogger(__name__)
 
 
-@dask.delayed
-def localize_state_chunkwise(state_grid, obs_grid, innov, hx_perts, obs_cov,
-                             localization):
-    if isinstance(state_grid, Future):
-        state_grid = state_grid.result()
-    localized_states = [
-        localize_states(
-            grid_l, obs_grid, innov, hx_perts, obs_cov, localization
-        )
-        for grid_l in state_grid
-    ]
-    return localized_states
-
-
-@dask.delayed
-def gen_weights_chunkwise(localized_states, back_prec, gen_weights_func):
-    weights = []
-    for state_l in localized_states:
-        w_mean_l, w_perts_l = gen_weights_func(back_prec, *state_l)
-        weights.append((w_mean_l + w_perts_l).t())
-    weights = torch.stack(weights, dim=0).detach()
-    return weights
-
-
-@dask.delayed
-def apply_weights_chunkwise(back_state, weights):
-    ana_perts = torch.einsum(
-        'ijkl,lkm->ijml', back_state, weights,
-    ).detach()
-    return ana_perts
-
-
-class DistributedLETKFCorr(LETKFCorr):
-    """
-    This is a dask-based implementation of the `localized ensemble transform
-    Kalman filter` :cite:`hunt_efficient_2007` for correlated observations.
-    This dask-based implementation is based on
-    :py:class:``~dask.distributed.Client``.
-
-    Parameters
-    ----------
-    client : :py:class:``~dask.distributed.Client`` or None
-        This dask distributed client is used to parallelize the processes.
-        Either this client or ``cluster`` has to be
-        specified. Default is None. If both, cluster and client, are given, then
-        client has priority.
-    cluster : compatible to :py:class:``~dask.disributed.LocalCluster`` or None
-        This dask cluster is used to initialize a :py:class:``~dask.distributed.
-        Client``, if no client is specified.
-        Either this cluster or a ``client`` has to be given. Default is None.
-    chunksize : int, optional
-        The data is splitted up such that every chunk has this number of
-        samples. This influences the performance of this distributed version of
-        the LETKF. Default is 10.
-    localization : obj or None, optional
-        This localization is used to localize and constrain observations
-        spatially. If this localization is None, no localization is applied such
-        it is an inefficient version of the `ensemble transform Kalman filter`.
-        Default value is None, indicating no localization at all.
-    inf_factor : float, optional
-        Multiplicative inflation factor :math:`\\rho``, which is applied to the
-        background precision. An inflation factor greater one increases the
-        ensemble spread, while a factor less one decreases the spread. Default
-        is 1.0, which is the same as no inflation at all.
-    smoother : bool, optional
-        Indicates if this filter should be run in smoothing or in filtering
-        mode. In smoothing mode, no analysis time is selected from given state
-        and the ensemble weights are applied to the whole state. In filtering
-        mode, the weights are applied only on selected analysis time. Default
-        is False, indicating filtering mode.
-    gpu : bool, optional
-        Indicator if the weight estimation should be done on either GPU (True)
-        or CPU (False): Default is None. For small models, estimation of the
-        weights on CPU is faster than on GPU!.
-    """
+class _DistributedLETKFBase(_LETKFBase):
     def __init__(self, client=None, cluster=None, chunksize=10,
                  localization=None, inf_factor=1.0, smoother=True,
                  gpu=False, pre_transform=None, post_transform=None):
@@ -214,57 +139,93 @@ class DistributedLETKFCorr(LETKFCorr):
         """
         logger.info('####### DISTRIBUTED LETKF #######')
         logger.info('Starting with applying observation operator')
-        innov, hx_perts, obs_cov, obs_grid = self._prepare(pseudo_state,
-                                                           observations)
-        logger.info('Chunking background state')
-        state_mean, state_perts = state.state.split_mean_perts()
-        state_perts = state_perts.chunk(
-            {'grid': self.chunksize, 'var_name': -1, 'time': -1, 'ensemble': -1}
+        pseudo_obs, obs_state, obs_cov, obs_grid = self._get_states(
+            pseudo_state, observations,
         )
 
         logger.info('Scatter the data to processes')
-        ens_mems = len(state.ensemble)
-        back_prec = self._get_back_prec(ens_mems)
-        innov, hx_perts, obs_cov = self._states_to_torch(
-            innov, hx_perts, obs_cov,
+        pseudo_obs, obs_state, obs_cov = self._states_to_torch(
+            pseudo_obs, obs_state, obs_cov
         )
-        state_perts_data = state_perts.data
-        state_grid = da.from_array(
-            state_perts.grid.values, chunks=self.chunksize
+
+        logger.info('Normalise perturbations and observations')
+        normed_perts, normed_obs = self._centre_tensors(pseudo_obs, obs_state)
+        obs_cinv = self._get_chol_inverse(obs_cov)
+        normed_perts, normed_obs, obs_cinv, obs_grid = self.client.scatter(
+            [normed_perts, normed_obs, obs_cinv, obs_grid], broadcast=True
         )
-        persisted_computes = self.client.persist([state_perts_data, state_grid])
-        state_perts_data, state_grid = self.client.gather(persisted_computes)
-        wait([state_perts_data, state_grid])
 
-        ana_perts = []
-        for k, grid_block in enumerate(state_grid.blocks):
-            localized_states = localize_state_chunkwise(
-                grid_block, obs_grid, innov, hx_perts, obs_cov,
-                self.localization
-            )
-            weights_l = gen_weights_chunkwise(localized_states, back_prec,
-                                              self._gen_weights_func)
-            torch_perts = dask.delayed(torch.as_tensor)(
-                state_perts_data.blocks[..., k], dtype=weights_l.dtype
-            )
-            ana_perts_l = apply_weights_chunkwise(
-                torch_perts, weights_l
-            )
-            ana_perts.append(ana_perts_l.detach().numpy())
-        ana_perts = dask.delayed(da.concatenate)(ana_perts, axis=-1)
-
+        logger.info('Chunking background state')
+        state = state.chunk(
+            {'grid': self.chunksize, 'var_name': -1, 'time': -1, 'ensemble': -1}
+        )
+        state_mean, state_perts = state.state.split_mean_perts()
         logger.info('Create analysis perturbations')
-        ana_perts = state_perts.copy(data=ana_perts.compute())
-
-        logger.info('Create analysis')
-        analysis = (ana_perts + state_mean).load()
+        ana_perts = xr.map_blocks(
+            self._localised_analysis,
+            obj=state_perts,
+            args=(normed_perts, normed_obs, obs_cinv, obs_grid),
+            template=state_perts
+        )
+        logger.info('Add background mean to analysis perturbations')
+        analysis = ana_perts + state_mean
+        logger.info('Compute analysis')
+        analysis = analysis.compute()
         return analysis
 
 
-class DistributedLETKFUncorr(DistributedLETKFCorr):
+class DistributedLETKFCorr(_CorrMixin, _DistributedLETKFBase):
     """
-    This is a MPI based implementation of the `localized ensemble transform
+    This is a dask-based implementation of the `localized ensemble transform
+    Kalman filter` :cite:`hunt_efficient_2007` for correlated observations.
+    This dask-based implementation is based on
+    :py:class:``~dask.distributed.Client``.
+
+    Parameters
+    ----------
+    client : :py:class:``~dask.distributed.Client`` or None
+        This dask distributed client is used to parallelize the processes.
+        Either this client or ``cluster`` has to be
+        specified. Default is None. If both, cluster and client, are given, then
+        client has priority.
+    cluster : compatible to :py:class:``~dask.disributed.LocalCluster`` or None
+        This dask cluster is used to initialize a :py:class:``~dask.distributed.
+        Client``, if no client is specified.
+        Either this cluster or a ``client`` has to be given. Default is None.
+    chunksize : int, optional
+        The data is splitted up such that every chunk has this number of
+        samples. This influences the performance of this distributed version of
+        the LETKF. Default is 10.
+    localization : obj or None, optional
+        This localization is used to localize and constrain observations
+        spatially. If this localization is None, no localization is applied such
+        it is an inefficient version of the `ensemble transform Kalman filter`.
+        Default value is None, indicating no localization at all.
+    inf_factor : float, optional
+        Multiplicative inflation factor :math:`\\rho``, which is applied to the
+        background precision. An inflation factor greater one increases the
+        ensemble spread, while a factor less one decreases the spread. Default
+        is 1.0, which is the same as no inflation at all.
+    smoother : bool, optional
+        Indicates if this filter should be run in smoothing or in filtering
+        mode. In smoothing mode, no analysis time is selected from given state
+        and the ensemble weights are applied to the whole state. In filtering
+        mode, the weights are applied only on selected analysis time. Default
+        is False, indicating filtering mode.
+    gpu : bool, optional
+        Indicator if the weight estimation should be done on either GPU (True)
+        or CPU (False): Default is None. For small models, estimation of the
+        weights on CPU is faster than on GPU!.
+    """
+    pass
+
+
+class DistributedLETKFUncorr(_UnCorrMixin, _DistributedLETKFBase):
+    """
+    This is a dask-based implementation of the `localized ensemble transform
     Kalman filter` :cite:`hunt_efficient_2007` for uncorrelated observations.
+    This dask-based implementation is based on
+    :py:class:``~dask.distributed.Client``.
 
     Parameters
     ----------
@@ -291,13 +252,4 @@ class DistributedLETKFUncorr(DistributedLETKFCorr):
         or CPU (False): Default is None. For small models, estimation of the
         weights on CPU is faster than on GPU!.
     """
-    def __init__(self, client=None, cluster=None, chunksize=10,
-                 localization=None, inf_factor=1.0,  smoother=True, gpu=False,
-                 pre_transform=None, post_transform=None):
-        super().__init__(client=client, cluster=cluster, chunksize=chunksize,
-                         localization=localization, inf_factor=inf_factor,
-                         smoother=smoother, gpu=gpu,
-                         pre_transform=pre_transform,
-                         post_transform=post_transform)
-        self._gen_weights_func = gen_weights_uncorr
-        self._correlated = False
+    pass
