d575c9a0fa532dc68e8af16dd4211e64b1244e29
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2020-12-20 13:48:34 +0100

    Added returning of state_index and state_array in localization for observations

--- pytassim/interface/mixin_local.py
+++ pytassim/interface/mixin_local.py
@@ -12,26 +12,55 @@
 
 # System modules
 import logging
+from typing import Tuple
 
 # External modules
 import xarray as xr
+import pandas as pd
 import numpy as np
 
 # Internal modules
-from .utils import index_to_array
+import pytassim.utilities.pandas as utils
 
 
 logger = logging.getLogger(__name__)
 
 
 class DomainLocalizedMixin(object):
+    def __init__(self):
+        self.localization = None
+
     @staticmethod
-    def _extract_obs_information(observations: xr.DataArray) -> np.ndarray:
-        obs_infos = index_to_array(observations['obs_id'].data)
-        return obs_infos
+    def _extract_obs_information(observations: xr.DataArray) -> pd.DataFrame:
+        obs_info = utils.multiindex_to_frame(observations.indexes['obs_id'])
+        return obs_info
 
-    def _extract_state_information(self, state):
-        pass
+    @staticmethod
+    def _extract_state_information(
+            state: xr.DataArray
+    ) -> Tuple[pd.MultiIndex, xr.DataArray]:
+        state_id = state.assign_coords(
+            time=utils.dtindex_to_total_seconds(state.indexes['time'])
+        )
+        state_id = state_id.state.stack(state_id=['time', 'grid'])
+        state_index = state_id.indexes['state_id']
+        state_array = utils.index_to_array(state_id.indexes['state_id'])
+        state_array = xr.DataArray(
+            state_array,
+            coords={
+                'state_id': np.arange(state_array.shape[0]),
+                'id_names': np.arange(state_array.shape[1])
+            },
+            dims=['state_id', 'id_names']
+        )
+        return state_index, state_array
 
-    def apply_dlocal_ufunc(self):
-        pass
+    def localization_decorator(self, func):
+        def wrapper(grid_info, *args, obs_info=None, **kwargs):
+            luse, lweights = self.localization.localize_obs(
+                grid_info, obs_info
+            )
+            lweights = lweights[luse]
+            args = [arg[..., luse]*lweights for arg in args]
+            return func(*args, **kwargs)
+        return wrapper
