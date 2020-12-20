c7898a7469f5806c6e4a536e3798566175d9cd56
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2020-12-20 10:45:32 +0100

    Unified function for index_to_array and datetimeindex_to_float in favor for multiindex to frame

--- pytassim/interface/utils.py
+++ pytassim/interface/utils.py
@@ -38,41 +38,23 @@ import numpy as np
 logger = logging.getLogger(__name__)
 
 
-def datetimeindex_to_float(index: Any) -> pd.Index:
-    float_index = pd.to_numeric(index, downcast='float')
-    return float_index
-
-
-def index_to_array(
-        index: Any
-) -> np.ndarray:
+def multiindex_to_frame(multiindex: pd.MultiIndex) -> pd.DataFrame:
     """
-    Transform a given index into a :py:class:`np.ndarray`, which can be then
-    used by localization within the assimilation.
+    Function to convert a `pandas.MultiIndex` into a `pandas.DataFrame`.
+    The automatically created index of the dataframe is dropped in favor of
+    an integer-based index.
 
     Parameters
     ----------
-    index : any
-        This index is transformed into an array.
+    multiindex : pd.MultiIndex
+        This multiindex is converted into a dataframe. The names of the
+        multiindex are used as columns.
 
     Returns
     -------
-    index_array : :py:class:`np.ndarray` (n_points, n_grid)
-        This is the transformmed array, which is then used within the
-        assimilation.
+    index_frame : pd.DataFrame
+        The constructed dataframe based on given multiindex.
     """
-    if isinstance(index, np.ndarray):
-        raw_index_array = index
-    elif isinstance(index, da.Array):
-        raw_index_array = index.compute()
-    else:
-        raw_index_array = np.atleast_1d(index.values)
-    if isinstance(raw_index_array[0], tuple):
-        shape = (-1, len(raw_index_array[0]))
-    elif raw_index_array.ndim > 1:
-        shape = raw_index_array.shape
-    else:
-        shape = (-1, 1)
-    dtype = ','.join(['float']*shape[-1])
-    index_array = np.array(index, dtype=dtype).view(float).reshape(*shape)
-    return index_array
+    index_frame = multiindex.to_frame()
+    index_frame = index_frame.reset_index(drop=True)
+    return index_frame
