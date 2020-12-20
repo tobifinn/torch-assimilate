450f941fa7ecc46f287d2879c99a0f0069058462
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2020-12-20 13:27:08 +0100

    Added conversion function from pandas index into numpy array

--- pytassim/utilities/pandas.py
+++ pytassim/utilities/pandas.py
@@ -12,9 +12,12 @@
 
 # System modules
 import logging
+from typing import Any
 
 # External modules
 import pandas as pd
+import numpy as np
+import dask.array as da
 
 # Internal modules
 
@@ -62,3 +65,38 @@ def multiindex_to_frame(multiindex: pd.MultiIndex) -> pd.DataFrame:
     index_frame = multiindex.to_frame()
     index_frame = index_frame.reset_index(drop=True)
     return index_frame
+
+
+def index_to_array(
+        index: Any
+) -> np.ndarray:
+    """
+    Transform a given index into a :py:class:`np.ndarray`, which can be then
+    used by localization within the assimilation.
+
+    Parameters
+    ----------
+    index : any
+        This index is transformed into an array.
+
+    Returns
+    -------
+    index_array : :py:class:`np.ndarray` (n_points, n_grid)
+        This is the transformmed array, which is then used within the
+        assimilation.
+    """
+    if isinstance(index, np.ndarray):
+        raw_index_array = index
+    elif isinstance(index, da.Array):
+        raw_index_array = index.compute()
+    else:
+        raw_index_array = np.atleast_1d(index.values)
+    if isinstance(raw_index_array[0], tuple):
+        shape = (-1, len(raw_index_array[0]))
+    elif raw_index_array.ndim > 1:
+        shape = raw_index_array.shape
+    else:
+        shape = (-1, 1)
+    dtype = ','.join(['float']*shape[-1])
+    index_array = np.array(index, dtype=dtype).view(float).reshape(*shape)
+    return index_array
