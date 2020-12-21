30068d89de9e398010dd4fb062c18d7d0fc9060b
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2020-12-21 14:51:15 +0100

    Removed deprecated `assimilation` submodule

--- pytassim/assimilation/filter/__init__.py
+++
@@ -1,6 +0,0 @@
-from .etkf import *
-from .letkf import *
-from .letkf_dist import *
-
-__all__ = ['ETKFCorr', 'ETKFUncorr', 'LETKFUncorr', 'LETKFCorr',
-           'DistributedLETKFCorr', 'DistributedLETKFUncorr']
