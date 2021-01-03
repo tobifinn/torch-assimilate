55fc93afc0fcdd41bcbd4936dae77fb6ec54f4d1
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-01-03 14:06:38 +0100

    Added singular value decomposition

--- pytassim/core/utils.py
+++ pytassim/core/utils.py
@@ -61,6 +61,63 @@ def evd(
     return evals, evects, evals_inv
 
 
+def svd(
+        tensor: torch.Tensor,
+        reg_value: float = 0.0
+) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """
+    Performs singular value decomposition of a tensor. The regularization
+    value is added to the tensor.
+
+    Parameters
+    ----------
+    tensor : :py:class:`torch.Tensor`
+        This tensor is decomposed.
+    reg_value : float, optional
+        This regularization value is added to the singular values and represents
+        a regularization of the matrix diagonal. The regularization is
+        deactivated with the default value of 0.
+
+    Returns
+    -------
+    u : :py:class:`torch.Tensor`
+        The decomposed left singular vector.
+    s : :py:class:`torch.Tensor`
+        The decomposed singular values with added regularization value.
+    v : :py:class:`torch.Tensor`
+        The decomposed right singular vector.
+    """
+    u, s, v = torch.svd(tensor)
+    s = s + reg_value
+    return u, s, v
+
+
+def rev_svd(
+        u: torch.Tensor,
+        s: torch.Tensor,
+        v: torch.Tensor
+) -> torch.Tensor:
+    """
+    Reverses a singular value decomposition.
+
+    Parameters
+    ----------
+    u : :py:class:`torch.Tensor`
+        The decomposed left singular vector.
+    s : :py:class:`torch.Tensor`
+        The decomposed singular values with added regularization value.
+    v : :py:class:`torch.Tensor`
+        The decomposed right singular vector.
+
+    Returns
+    -------
+    composed_tensor : :py:class:`torch.Tensor`
+        The recomposed tensor from given ``u``, ``s``, and ``v``.
+    """
+    composed_tensor = torch.matmul(u * s, v.transpose(-1, -2))
+    return composed_tensor
+
+
 def rev_evd(
         evals: torch.Tensor,
         evects: torch.Tensor,
