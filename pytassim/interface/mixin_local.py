d801dc1ad9c93fed96491c844862e563a747b487
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-01-04 14:42:39 +0100

    Added skipping of argument if args_to_skip specified

--- pytassim/interface/mixin_local.py
+++ pytassim/interface/mixin_local.py
@@ -30,13 +30,18 @@ logger = logging.getLogger(__name__)
 class DomainLocalizedMixin(object):
     @property
     def localized_module(self):
-        def wrapper(grid_info, *args, obs_info=None):
+        def wrapper(grid_info, *args, obs_info=None, args_to_skip=None):
             if self.localization is not None:
                 luse, lweights = self.localization.localize_obs(
                     grid_info, obs_info
                 )
                 lweights = np.sqrt(lweights[luse])
-                args = [arg[..., luse]*lweights for arg in args]
+                if args_to_skip is None:
+                    args_to_skip = []
+                args = [
+                    arg if k in args_to_skip else arg[..., luse] * lweights
+                    for k, arg in enumerate(args)
+                ]
             return self.module(*args)
         return wrapper
 
