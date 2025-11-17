91b9838ea11087e18ca8e4794bd49efce172bb15
tobifinn <tobias.finn@enpc.fr>
2025-11-17 14:53:21 +0100

    Added new method for weight interpolated LETKF and benchmark example

---
+++ examples/benchmark_interp.py
@@ -0,0 +1,190 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Created on 11/17/2025
+#
+# Created for torch-assimilate
+#
+# @author: Tobias Sebastian Finn, tobias.finn@enpc.fr
+#
+#    Copyright (C) {2025}  {Tobias Sebastian Finn}
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# System modules
+import logging
+import datetime
+import time
+import argparse
+
+# External modules
+import xarray as xr
+import numpy as np
+import dask
+
+# Internal modules
+import pytassim
+from pytassim.interface import WeightInterpLETKF
+from pytassim.localization import GaspariCohn
+from pytassim.obs_ops.base_ops import BaseOperator
+
+
+logger = logging.getLogger(__name__)
+
+rnd = np.random.RandomState(42)
+
+parser = argparse.ArgumentParser(description='LETKF Benchmark')
+parser.add_argument(
+    '-k', '--ens_size',
+    help='The number of ensemble members',
+    type=int, default=40
+)
+parser.add_argument(
+    '-l', '--len_grid',
+    help='Length of state grid', type=int, default=10000
+)
+parser.add_argument(
+    '-n', '--nr_obs',
+    help='Number of observations (should be less/equal than state grid length)',
+    type=int, default=1000
+)
+parser.add_argument(
+    '-r', '--loc_radius',
+    help='Localization radius in grid points',
+    type=int, default=20
+)
+parser.add_argument(
+    '-w', '--num_workers',
+    help='Number of parallel workers in dask',
+    type=int, default=1
+)
+
+
+def distance_func(x_grid, y_grid):
+    # Dimensions (time, grid)
+    # Select only grid information for distance estimation
+    dist = np.abs(x_grid[1]-y_grid["obs_grid_1"].values)
+    return dist
+
+
+class IdentityOperator(BaseOperator):
+    def __init__(self, len_grid, nr_obs):
+        super().__init__(len_grid=len_grid)
+        self.nr_obs = nr_obs
+
+    @property
+    def obs_grid(self):
+        return np.linspace(start=0, stop=self.len_grid, num=self.nr_obs,
+                           endpoint=False)
+
+    def obs_op(self, in_array, *args, **kwargs):
+        if 'var_name' in in_array.dims:
+            in_array = in_array.sel(var_name='x')
+        obs_state = in_array.sel(grid=self.obs_grid, method='nearest')
+        return obs_state
+
+
+def get_state_data(len_grid=10000, ens_size=50):
+    grid_range = np.arange(len_grid)
+    ens_range = np.arange(ens_size)
+
+    data = rnd.normal(size=(1, 1, ens_size, len_grid))
+    state_array = xr.DataArray(
+        data=data,
+        coords={
+            'var_name': ['x', ],
+            'time': [datetime.datetime(1992, 12, 25, 8), ],
+            'ensemble': ens_range,
+            'grid': grid_range
+        },
+        dims=['var_name', 'time', 'ensemble', 'grid']
+    )
+    return state_array
+
+
+def get_truth(len_grid=10000):
+    truth = xr.DataArray(
+        rnd.normal(size=(1, len_grid)),
+        coords={
+            'time': [datetime.datetime(1992, 12, 25, 8), ],
+            'grid': np.arange(len_grid)
+        },
+        dims=['time', 'grid']
+    )
+    return truth
+
+
+def get_obs_data(truth: xr.DataArray, nr_obs=1000):
+    grid_range = np.linspace(
+        start=0, stop=len(truth["grid"]), num=nr_obs, endpoint=False
+    )
+    obs_data = truth.sel(grid=grid_range)
+    obs_data = obs_data.rename({"grid": "obs_grid_1"})
+    obs_cov = xr.DataArray(
+        data=[1, ] * nr_obs,
+        coords={
+            'obs_grid_1': grid_range
+        },
+        dims=['obs_grid_1']
+    )
+    observations = xr.Dataset(
+        {
+            'observations': obs_data,
+            'covariance': obs_cov
+        }
+    )
+    return observations
+
+
+def main(
+        len_grid=10000, nr_obs=1000, ens_size=50, loc_radius=20, num_workers=1
+):
+    truth = get_truth(len_grid)
+    back_state = get_state_data(len_grid, ens_size)
+    weight_grid = xr.Dataset(
+        coords={
+            "time": back_state.indexes["time"],
+            "grid": back_state.indexes["grid"][::20]
+        }
+    )
+    obs_state = get_obs_data(truth, nr_obs)
+    obs_operator = IdentityOperator(len_grid=len_grid, nr_obs=nr_obs)
+    obs_state.obs.operator = obs_operator
+
+    localization = GaspariCohn(length_scale=loc_radius, dist_func=distance_func)
+    letkf = WeightInterpLETKF(
+        localization=localization, inf_factor=1.1, weight_grid=weight_grid
+    )
+    start_time = time.time()
+    with dask.config.set(num_workers=num_workers): 
+        analysis = letkf.assimilate(back_state, obs_state)
+        analysis = analysis.compute()
+    logger.info(
+        'Assimilation duration: {0:.2f} s'.format(time.time() - start_time)
+    )
+    
+    bg_rmse = np.sqrt(((back_state.mean("ensemble")-truth)**2).mean())
+    logger.info("Background RMSE: {0:.4f}".format(bg_rmse))
+    ana_rmse = np.sqrt(((analysis.mean("ensemble")-truth)**2).mean())
+    logger.info("Analysis RMSE: {0:.4f}".format(ana_rmse))
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.INFO)
+    main(
+        len_grid=args.len_grid, nr_obs=args.nr_obs, ens_size=args.ens_size,
+        loc_radius=args.loc_radius, num_workers=args.num_workers
+    )
---
+++ pytassim/interface/interp_letkf.py
@@ -0,0 +1,103 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Created on 17.11.2025
+#
+# Created for torch-assimilate
+#
+# @author: Tobias Sebastian Finn, tobias.finn@enpc.fr
+#    Copyright (C) {2025}  {Tobias Sebastian Finn}
+
+# System modules
+from typing import Iterable, Callable, Tuple, Any
+
+# External modules
+from pytassim.localization.localization import BaseLocalization
+from pytassim.transform.base import BaseTransformer
+import xarray as xr
+import pandas as pd
+
+from scipy.interpolate import LinearNDInterpolator, interp1d
+
+# Internal modules
+from pytassim.interface.letkf import LETKF
+
+
+class WeightInterpLETKF(LETKF):
+    def __init__(
+            self,
+            localization: None | BaseLocalization = None,
+            inf_factor: float | Any = 1,
+            smoother: bool = False,
+            gpu: bool = False,
+            pre_transform: None | Iterable[BaseTransformer] = None,
+            post_transform: None | Iterable[BaseTransformer] = None,
+            chunksize: int = 10,
+            weight_save_path: None | str = None,
+            forward_model: None | Callable[..., Any] = None,
+            weight_grid: None | xr.DataArray = None,
+    ):
+        super().__init__(
+            localization,
+            inf_factor,
+            smoother,
+            gpu,
+            pre_transform,
+            post_transform,
+            chunksize,
+            weight_save_path,
+            forward_model
+        )
+        self.weight_grid = weight_grid
+        
+    def _extract_state_information(
+            self, state: xr.DataArray
+    ) -> Tuple[pd.MultiIndex, xr.DataArray]:
+        if self.weight_grid is not None:
+            return super()._extract_state_information(self.weight_grid)
+        return super()._extract_state_information(state)
+    
+    def _interpolate_weights(
+            self,
+            state: xr.DataArray,
+            weights: xr.DataArray,
+    ) -> xr.DataArray:
+        w_coords = weights.indexes["grid"]
+        s_coords = state.indexes["grid"]        
+        # ----- 1‑D vs ≥2‑D branch -----
+        if isinstance(w_coords, pd.MultiIndex):
+            interp = LinearNDInterpolator(
+                w_coords.to_frame(index=False).values,
+                weights.values,
+                fill_value=0.0,
+                rescale=True,
+            )
+            interp_weights = interp(s_coords.to_frame(index=False).values)
+        else:
+            # 1D values
+            interp = interp1d(
+                w_coords.values,
+                weights.values,
+                kind='linear',
+                bounds_error=False,
+                fill_value=0.0,
+                axis=0
+            )
+            interp_weights = interp(s_coords.values)
+        interp_weights = xr.DataArray(
+            interp_weights,
+            dims=["grid", "ensemble", "ensemble_new"],
+            coords={
+                "grid": state.indexes["grid"],
+                "ensemble": weights.indexes["ensemble"]
+            }
+        )
+        return interp_weights
+    
+    def _apply_weights(
+            self,
+            state: xr.DataArray,
+            weights: xr.DataArray
+    ) -> xr.DataArray:
+        interp_weights = self._interpolate_weights(state, weights)
+        return super()._apply_weights(state, interp_weights)
